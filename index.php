f4fbf0c19d761083fbd4db695175cb26713f52cc
metazo <metaz0412@gmail.com>
2022-04-12 11:00:27 +0200

    Afficher les membres du site

--- index.php
+++ index.php
@@ -3,5 +3,17 @@ session_start();
 if(!$_SESSION['mdp']) {
     header('Location: connexion.php');
 }
-
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+</head>
+<body>
+    <a href="./membres.php">Afficher tous les membres</a>
+</body>
+</html>
---
+++ membres.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+$bdd = new PDO('mysql:host=localhost;dbname=espace_admin', 'root' , 'root');
+if(!$_SESSION['mdp']) {
+    header('Location: connexion.php');
+}   
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Afficher tous les membres</title>
+</head>
+<body>
+    <!-- Afficher tous les membres -->
+    <?php
+    $recupUsers = $bdd->query('SELECT * FROM membres');
+    while($user = $recupUsers->fetch()){
+        ?>
+        <p><?= $user['pseudo']; ?></p>
+        <?php
+    }
+    ?>
+    <!-- Fin d'afficher tous les membres -->
+</body>
+</html>
